c3849bb23b551501d6cc5397a3f1abc06643fe6b
as6620 <as6620@bs.amalnet.k12.il>
2024-02-10 16:57:19 +0200

    second

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    WebView wV;
+    EditText eT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        wV = findViewById(R.id.wV);
+        wV.getSettings().setJavaScriptEnabled(true);
+        eT = findViewById(R.id.eT);
     }
+
+    public void Clicked(View view) {
+        
+    }
+
 }
